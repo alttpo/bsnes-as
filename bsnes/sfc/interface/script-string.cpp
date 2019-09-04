d36b2b5fbc69e0bc742416788dd90b9a90702708
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2019-09-03 23:43:34 -0500

    Add support for concatenating strings, comparing strings, and equality-checking strings.

--- bsnes/sfc/interface/script-string.cpp
+++ bsnes/sfc/interface/script-string.cpp
@@ -42,26 +42,45 @@ struct CNallStringFactory : public asIStringFactory {
 
 static CNallStringFactory *stringFactory = nullptr;
 
-CNallStringFactory *GetStdStringFactorySingleton() {
+static auto stringFactorySingleton() -> CNallStringFactory* {
   if(stringFactory == nullptr) {
     stringFactory = new CNallStringFactory();
   }
   return stringFactory;
 }
 
-static void stringConstruct(string *thisPointer) {
+static auto stringConstruct(string *thisPointer) -> void {
   new(thisPointer) string();
 }
 
-static void stringCopyConstruct(const string &other, string *thisPointer) {
+static auto stringCopyConstruct(const string &other, string *thisPointer) -> void {
   new(thisPointer) string(other);
 }
 
-static void stringDestruct(string *thisPointer) {
+static auto stringDestruct(string *thisPointer) -> void {
   thisPointer->~string();
 }
 
-static string *stringHex(uint64_t value, int precision = 0) {
+static auto stringAddAssign(const string *other, string *self) -> string* {
+  self->_append<string>(*other);
+  return self;
+}
+
+static auto stringEquals(const string *self, const string *other) -> bool {
+  return *self == *other;
+}
+
+static auto stringCmp(const string *self, const string *other) -> int {
+  return self->compare(*other);
+}
+
+static auto stringAdd(const string *self, const string *other) -> string* {
+  string *dest = new string(*self);
+  dest->_append<string>(*other);
+  return dest;
+}
+
+static auto stringHex(uint64_t value, int precision = 0) -> string* {
   return new string(hex(value, precision));
 }
 
@@ -69,13 +88,18 @@ auto Interface::registerScriptString() -> void {
   int r;
   // register string type:
   r = script.engine->RegisterObjectType("string", sizeof(string), asOBJ_VALUE | asOBJ_APP_CLASS_CDAK); assert( r >= 0 );
-  r = script.engine->RegisterStringFactory("string", GetStdStringFactorySingleton());
+  r = script.engine->RegisterStringFactory("string", stringFactorySingleton());
 
   // Register the object operator overloads
   r = script.engine->RegisterObjectBehaviour("string", asBEHAVE_CONSTRUCT,  "void f()",                    asFUNCTION(stringConstruct), asCALL_CDECL_OBJLAST); assert(r >= 0);
   r = script.engine->RegisterObjectBehaviour("string", asBEHAVE_CONSTRUCT,  "void f(const string &in)",    asFUNCTION(stringCopyConstruct), asCALL_CDECL_OBJLAST); assert(r >= 0);
   r = script.engine->RegisterObjectBehaviour("string", asBEHAVE_DESTRUCT,   "void f()",                    asFUNCTION(stringDestruct),  asCALL_CDECL_OBJLAST); assert(r >= 0);
+
   r = script.engine->RegisterObjectMethod("string", "string &opAssign(const string &in)", asMETHODPR(string, operator =, (const string&), string&), asCALL_THISCALL); assert(r >= 0);
+  r = script.engine->RegisterObjectMethod("string", "string &opAddAssign(const string &in)", asFUNCTION(stringAddAssign), asCALL_CDECL_OBJLAST); assert( r >= 0 );
+  r = script.engine->RegisterObjectMethod("string", "bool opEquals(const string &in) const", asFUNCTION(stringEquals), asCALL_CDECL_OBJFIRST); assert( r >= 0 );
+  r = script.engine->RegisterObjectMethod("string", "int opCmp(const string &in) const", asFUNCTION(stringCmp), asCALL_CDECL_OBJFIRST); assert( r >= 0 );
+  r = script.engine->RegisterObjectMethod("string", "string &opAdd(const string &in) const", asFUNCTION(stringAdd), asCALL_CDECL_OBJFIRST); assert( r >= 0 );
 
   // Register global functions
   r = script.engine->RegisterGlobalFunction("string &hex(uint64 value, int precision = 0)", asFUNCTION(stringHex), asCALL_CDECL); assert(r >= 0);
