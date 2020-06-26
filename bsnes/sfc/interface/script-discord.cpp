2ddc6eee819f0b9ebb763a0aadd12359eb28cccb
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2020-06-26 13:48:29 -0500

    Register manager getter properties.

--- bsnes/sfc/interface/script-discord.cpp
+++ bsnes/sfc/interface/script-discord.cpp
@@ -18,8 +18,9 @@ auto logCallback(discord::LogLevel level, const char* message) -> void {
 auto Register(asIScriptEngine *e) -> void {
   int r;
 
-#define REG_REF_TYPE(name) r = e->RegisterObjectType(#name, 0, asOBJ_REF); assert( r >= 0 )
 #define REG_REF_FLAGS(name, flags) r = e->RegisterObjectType(#name, 0, flags); assert( r >= 0 )
+#define REG_REF_TYPE(name) REG_REF_FLAGS(name, asOBJ_REF)
+#define REG_REF_NOCOUNT(name) REG_REF_FLAGS(name, asOBJ_REF | asOBJ_NOCOUNT)
 
 #define REG_GLOBAL(defn, ptr) r = e->RegisterGlobalProperty(defn, ptr); assert( r >= 0 )
 
@@ -67,6 +68,65 @@ auto Register(asIScriptEngine *e) -> void {
       }
 
       return static_cast<int>(result);
+    }));
+
+    // Managers:
+    REG_REF_NOCOUNT(ApplicationManager);
+    REG_REF_NOCOUNT(UserManager);
+    REG_REF_NOCOUNT(ImageManager);
+    REG_REF_NOCOUNT(ActivityManager);
+    REG_REF_NOCOUNT(RelationshipManager);
+    REG_REF_NOCOUNT(LobbyManager);
+    REG_REF_NOCOUNT(NetworkManager);
+    REG_REF_NOCOUNT(OverlayManager);
+    REG_REF_NOCOUNT(StorageManager);
+    REG_REF_NOCOUNT(StoreManager);
+    REG_REF_NOCOUNT(VoiceManager);
+    REG_REF_NOCOUNT(AchievementManager);
+
+    REG_LAMBDA_GLOBAL("ApplicationManager@ get_applicationManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::ApplicationManager*)nullptr;
+      return &core->ApplicationManager();
+    }));
+    REG_LAMBDA_GLOBAL("UserManager@ get_userManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::UserManager*)nullptr;
+      return &core->UserManager();
+    }));
+    REG_LAMBDA_GLOBAL("ImageManager@ get_imageManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::ImageManager*)nullptr;
+      return &core->ImageManager();
+    }));
+    REG_LAMBDA_GLOBAL("ActivityManager@ get_activityManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::ActivityManager*)nullptr;
+      return &core->ActivityManager();
+    }));
+    REG_LAMBDA_GLOBAL("RelationshipManager@ get_relationshipManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::RelationshipManager*)nullptr;
+      return &core->RelationshipManager();
+    }));
+    REG_LAMBDA_GLOBAL("LobbyManager@ get_lobbyManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::LobbyManager*)nullptr;
+      return &core->LobbyManager();
+    }));
+    REG_LAMBDA_GLOBAL("NetworkManager@ get_networkManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::NetworkManager*)nullptr;
+      return &core->NetworkManager();
+    }));
+    REG_LAMBDA_GLOBAL("OverlayManager@ get_overlayManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::OverlayManager*)nullptr;
+      return &core->OverlayManager();
+    }));
+    REG_LAMBDA_GLOBAL("StorageManager@ get_storageManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::StorageManager*)nullptr;
+      return &core->StorageManager();
+    }));
+    REG_LAMBDA_GLOBAL("StoreManager@ get_storeManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::StoreManager*)nullptr;
+      return &core->StoreManager();
+    }));
+    REG_LAMBDA_GLOBAL("VoiceManager@ get_voiceManager() property", ([](){
+      if (!core) return result = discord::Result::NotRunning, (discord::VoiceManager*)nullptr;
+      return &core->VoiceManager();
     }));
   }
 }
