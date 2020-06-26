0e3234a012105e71bf3dec23da6843ec7d27b09c
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2020-06-26 13:39:53 -0500

    Refactored to avoid single Core instance exposed to scripts so we can control its nullability.

--- bsnes/sfc/interface/script-discord.cpp
+++ bsnes/sfc/interface/script-discord.cpp
@@ -2,21 +2,30 @@
 namespace DiscordInterface {
 
 discord::Result result;
+discord::Core* core{};
+discord::LogLevel minLevel = discord::LogLevel::Debug;
+
+auto logCallback(discord::LogLevel level, const char* message) -> void {
+  static const char *levelNames[] = {
+    "ERROR",
+    "WARN",
+    "INFO",
+    "DEBUG"
+  };
+  platform->scriptMessage({"Discord ", levelNames[static_cast<int>(level)], ": ", message}, (level == discord::LogLevel::Error));
+}
 
 auto Register(asIScriptEngine *e) -> void {
   int r;
 
 #define REG_REF_TYPE(name) r = e->RegisterObjectType(#name, 0, asOBJ_REF); assert( r >= 0 )
+#define REG_REF_FLAGS(name, flags) r = e->RegisterObjectType(#name, 0, flags); assert( r >= 0 )
 
 #define REG_GLOBAL(defn, ptr) r = e->RegisterGlobalProperty(defn, ptr); assert( r >= 0 )
 
 #define REG_LAMBDA(name, defn, lambda) r = e->RegisterObjectMethod(#name, defn, asFUNCTION(+lambda), asCALL_CDECL_OBJFIRST); assert( r >= 0 )
 
 #define REG_LAMBDA_GLOBAL(defn, lambda) r = e->RegisterGlobalFunction(defn, asFUNCTION(+lambda), asCALL_CDECL); assert( r >= 0 )
-
-#define EXPOSE_SHARED_PTR(name, className) \
-  r = e->RegisterObjectBehaviour(#name, asBEHAVE_ADDREF,  "void f()", asFUNCTION(sharedPtrAddRef), asCALL_CDECL_OBJFIRST); assert( r >= 0 ); \
-  r = e->RegisterObjectBehaviour(#name, asBEHAVE_RELEASE, "void f()", asFUNCTION(+([](shared_pointer<className>& p){ sharedPtrRelease<className>(p); })), asCALL_CDECL_OBJFIRST); assert( r >= 0 )
 
 #define REG_LAMBDA_CTOR(name, defn, lambda) \
   r = e->RegisterObjectBehaviour(#name, asBEHAVE_FACTORY, defn, asFUNCTION(+lambda), asCALL_CDECL); assert( r >= 0 )
@@ -25,25 +34,40 @@ auto Register(asIScriptEngine *e) -> void {
     r = e->SetDefaultNamespace("discord"); assert(r >= 0);
 
     // Core type:
-    REG_REF_TYPE(Core);
-    EXPOSE_SHARED_PTR(Core, discord::Core);
+    //REG_REF_FLAGS(Core, asOBJ_REF | asOBJ_NOCOUNT);
 
     REG_GLOBAL("int result", &result);
+    REG_GLOBAL("const bool created", &core); // implicit nullptr -> false
 
-    // constructor:
-    REG_LAMBDA_CTOR(Core, "Core@ f(uint64 clientId, bool requireDiscordClient = false)", ([](uint64_t clientId, bool requireDiscordClient) {
-      discord::Core* core{};
-
+    // create():
+    REG_LAMBDA_GLOBAL("int create(uint64 clientId, bool requireDiscordClient = false)", ([](uint64_t clientId, bool requireDiscordClient) {
       result = discord::Core::Create(clientId, requireDiscordClient ? DiscordCreateFlags_Default : DiscordCreateFlags_NoRequireDiscord, &core);
-      if (core == nullptr) {
-        return (shared_pointer<discord::Core>*)nullptr;
-      }
+      if (!core) return static_cast<int>(result);
 
-      return new shared_pointer<discord::Core>(core);
+      core->SetLogHook(minLevel, logCallback);
+      return static_cast<int>(result);
     }));
 
-    // RunCallbacks()
-    REG_LAMBDA(Core, "int RunCallbacks()", ([](shared_pointer<discord::Core>&p){ return p->RunCallbacks(); }));
+    // set_logLevel:
+    REG_LAMBDA_GLOBAL("void set_logLevel(int level) property", ([](int level) {
+      minLevel = static_cast<discord::LogLevel>(level);
+      if (!core) return void(result = discord::Result::NotRunning);
+
+      core->SetLogHook(minLevel, logCallback);
+    }));
+
+    // runCallbacks()
+    REG_LAMBDA_GLOBAL("int runCallbacks()", ([]() {
+      if (!core) return static_cast<int>(result = discord::Result::NotRunning);
+
+      result = core->RunCallbacks();
+      if (result == discord::Result::NotRunning) {
+        delete core;
+        core = nullptr;
+      }
+
+      return static_cast<int>(result);
+    }));
   }
 }
 
--- test/discord.as
+++ test/discord.as
@@ -1,26 +1,25 @@
 
-discord::Core@ core = null;
 int lastConnect = 1;
 
 void pre_frame() {
-  if (core is null) {
+  if (!discord::created) {
     if (--lastConnect > 0) return;
 
     // attempt reconnect every 15 seconds:
-    lastConnect = 60 * 15;
-    @core = discord::Core(725722436351426643);
-    if (discord::result != 0) {
-      message("discord: connect failed with err=" + fmtInt(discord::result));
+    lastConnect = 60 * 10;
+
+    //discord::logLevel = 4;  // DEBUG
+    if (discord::create(725722436351426643) != 0) {
+      message("discord::create() failed with err=" + fmtInt(discord::result));
       return;
     }
 
     message("discord: connected!");
   }
-  if (core is null) return;
+  if (!discord::created) return;
 
-  core.RunCallbacks();
-  if (discord::result != 0) {
-    message("discord: RunCallbacks() failed with err=" + fmtInt(discord::result));
+  if (discord::runCallbacks() != 0) {
+    message("discord::runCallbacks() failed with err=" + fmtInt(discord::result));
     return;
   }
 
