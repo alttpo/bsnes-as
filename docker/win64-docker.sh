0369a75d294ca582149c38c35f712ae19d5af6da
James Dunne <james.jdunne@gmail.com>
2020-08-04 17:05:23 -0500

    docker: minor fix to win64-docker.sh script

--- docker/win64-docker.sh
+++ docker/win64-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker build -t clang-build-win64 clang-build-win64
+docker build -t clang-build-win64 docker/clang-build-win64
 docker volume create clang-win64-volume
 docker rm clang-win64
 docker create --name clang-win64 -v clang-win64-volume:/app -w /app -it clang-build-win64
