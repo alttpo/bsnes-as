211a30c031800894165d660b5b4e70375544c53c
James Dunne <james.jdunne@gmail.com>
2020-10-10 19:22:12 -0500

    gui: working value type for Window shared_pointer that creates window on init and destroys on unload automatically

--- bsnes/sfc/interface/script-gui.cpp
+++ bsnes/sfc/interface/script-gui.cpp
@@ -722,9 +722,16 @@ auto RegisterGUI(asIScriptEngine *e) -> void {
     // experimental:
     REG_VALUE_TYPE(Window2, hiro::Window, asOBJ_APP_CLASS_CDAK);
     //EXPOSE_VALUE_CDAK(Window2, hiro::Window);
-    REG_LAMBDA_CTOR(Window2, "void f()",                             ([](void* address){ value_construct<hiro::Window>(address); }));
+    REG_LAMBDA_CTOR(Window2, "void f()",                             ([](void* address){
+      value_construct<hiro::Window>(address);
+    }));
     REG_LAMBDA_CTOR(Window2, "void f(const Window2 &in)",            ([](void* address, hiro::Window* other){ value_copy_construct(address, other); }));
-    REG_LAMBDA_DTOR(Window2, "void f()",                             ([](hiro::Window* self){ value_destroy(self); }));
+    REG_LAMBDA_DTOR(Window2, "void f()",                             ([](hiro::Window& self){
+      self->setVisible(false);
+      self->setDismissable(true);
+      self->destruct();
+      self.reset();
+    }));
     REG_LAMBDA     (Window2, "Window2 &opAssign(const Window2 &in)", ([](hiro::Window* self, hiro::Window* other){ value_assign(self, other); }));
 
     EXPOSE_OBJECT(Window2, hiro::Window);
---
+++ test/window2.as
@@ -0,0 +1,6 @@
+GUI::Window2 w;
+
+void init() {
+  w.size = GUI::Size(400, 400);
+  w.visible = true;
+}
