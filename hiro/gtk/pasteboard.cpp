915f98611f8584329c9aa5c9d761deef690a1281
jsd1982 <james.jdunne@gmail.com>
2025-03-05 00:28:12 -0600

    hiro: fix gtk build

--- hiro/gtk/pasteboard.cpp
+++ hiro/gtk/pasteboard.cpp
@@ -4,7 +4,7 @@ namespace hiro {
 
 auto pPasteboard::pasteString(const string& text) -> void {
   auto display = gdk_display_get_default();
-  auto clipboard = gtk_clipboard_get_default(display);
+  auto clipboard = gtk_clipboard_get_for_display(display, GDK_SELECTION_CLIPBOARD);
   gtk_clipboard_set_text(clipboard, text, text.size());
 }
 
